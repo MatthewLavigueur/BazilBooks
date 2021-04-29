ee07203ca24f767f7aacb0f57534977bc029353f
matthewlavigueur <Matty.spades@gmail.com>
2021-04-28 22:03:21 -0400

    master.blade change footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -188,7 +188,7 @@
                 <footer class="py-2 bg-light mt-auto">
                     <div class="container-fluid">
                         <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Your Website 2020</div>
+                            <div class="text-muted">Copyright &copy; Bazil Books by Matthew Lavigueur 2021</div>
                             <div>
                                 <a href="#">Privacy Policy</a>
                                 &middot;
